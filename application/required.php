b63029029e4356a88449abf2e4994263890a7f5e
Stefan Göbel <goebel@gobilo.de>
2012-06-01 10:46:49 +0200

    dskfjhsdkj

---
+++ application/required.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * sjust a test2
+ */
+function required_test($output) 
+{
+    echo 'works: '.$output;
+}
+
+/**
+ * hallihallo does tod
+ * 
+ * @param string $dda
+ *   hhh
+ */
+function kanothertwo($dda) 
+{
+    echo 'jhfhg';
+}
